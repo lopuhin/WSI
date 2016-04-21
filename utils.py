be9f7618f46a305859a6e80177a650d38104db8c
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-04-21 23:37:19 +0300

    Better LDA with weights
    Although ARI is almost the same

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 import os.path
+import re
+
+import pymystem3
+
+
+MyStem = pymystem3.Mystem()
 
 
 def load_stopwords():
@@ -13,3 +19,19 @@ def load_contexts(root, word):
     with open(os.path.join(root, '{}.txt'.format(word))) as f:
         return [[w for w in line.strip().split()
                  if w not in stopwords and w != word] for line in f]
+
+
+word_re = re.compile(r'\w+', re.U)
+
+
+def normalize(ctx):
+    left, _, right = ctx
+    text = ' '.join([left, right]).strip()
+   #text = re.sub(r'\d', '2', text)
+    return [w for w in MyStem.lemmatize(' '.join(word_re.findall(text)))
+            if w not in stopwords and w.strip()]
+
+
+def load_weights(root, word):
+    with open(os.path.join(root, word + '.txt')) as f:
+        return {w: float(weight) for w, weight in (l.split() for l in f)}

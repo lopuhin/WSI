1d3ab41d871394d768d188524efd8b1f80f3ed5b
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-04-22 23:21:18 +0300

    Run multiple processes

--- lda.py
+++ lda.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import logging
+from collections import defaultdict
+from concurrent.futures import ProcessPoolExecutor
 
 import numpy as np
 from gensim import corpora
@@ -12,23 +14,20 @@ from sklearn.metrics import v_measure_score, adjusted_rand_score
 import utils
 
 
-def word_lda(word, num_topics=6):
+def word_lda(word, num_topics=6, limit=None):
     weights = utils.load_weights('../corpora/ad-nouns/cdict/', word)
     texts = utils.load_contexts('../corpora/ad-nouns-contexts-100k', word)
     texts = [[w for w in ctx if weights.get(w, 0) > 1] for ctx in texts]
     texts = [ctx for ctx in texts if ctx]
+    if limit:
+        texts = texts[:limit]
     dictionary = corpora.Dictionary(texts)
     corpus = [dictionary.doc2bow(text) for text in texts]
 
-    print(len(corpus))
     #lda = HdpModel(corpus, id2word=dictionary)
     lda = LdaModel(
         corpus, id2word=dictionary, num_topics=num_topics,
         passes=3, iterations=100, alpha='auto')
-
-    for topic_id in range(lda.num_topics):
-        terms = lda.get_topic_terms(topic_id, topn=5)
-        print(topic_id, ' '.join(dictionary[wid] for wid, _ in terms))
 
     _senses, contexts = rl_wsd_labeled.get_contexts(
         rl_wsd_labeled.contexts_filename('nouns', 'RuTenTen', word))
@@ -40,15 +39,30 @@ def word_lda(word, num_topics=6):
 
     ari = adjusted_rand_score(true_labels, pred_topics)
     v_score = v_measure_score(true_labels, pred_topics)
-    return ari, v_score
+    return lda, dictionary, ari, v_score
 
 
-def check_stability():
-    for word in ['горшок']:
+def print_topics(lda, dictionary):
+    for topic_id in range(lda.num_topics):
+        terms = lda.get_topic_terms(topic_id, topn=5)
+        print(topic_id, ' '.join(dictionary[wid] for wid, _ in terms))
+
+
+def run_all(n_runs=3, limit=None):
+    print('limit', limit)
+    futures = []
+    with ProcessPoolExecutor(max_workers=4) as e:
+        for word in words_1:
+            futures.extend((word, e.submit(word_lda, word, limit=limit))
+                           for _ in range(n_runs))
+    results_by_word = defaultdict(list)
+    for word, f in futures:
+        results_by_word[word].append(f.result())
+    for word, results in results_by_word.items():
         print()
         print(word)
-        for _ in range(3):
-            ari, v_score = word_lda(word)
+        for lda, dictionary, ari, v_score in results:
+            print_topics(lda, dictionary)
             print('ARI: {:.3f}, V-score: {:.3f}'.format(ari, v_score))
 
 
@@ -62,9 +76,11 @@ words_1 = [
     'горшок',
     ]
 
+
 def main():
    #logging.basicConfig(level=logging.INFO)
-    check_stability()
+    run_all(limit=30000)
+
 
 if __name__ == '__main__':
     main()
